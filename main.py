b80b457c9a475664ce8dbac65889c8531be89aae
ron93 <ronmkare1@gmail.com>
2021-02-07 09:35:00 +0300

    plant disease ditector endpoint -- /analyze:predict disease from pic and /info:get disease info

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/analyze")
+@app.post("/analyze")
 def analyze():
     return {"prediction": "Disease ditector!"}
+
+@app.get("/info")
+def get_disease():
+    return {"Info": "Disease information"}
