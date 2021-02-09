b8c0ec52466f745e06418afa24c2d1fb8954159b
ron93 <ronmkare1@gmail.com>
2021-02-10 00:14:31 +0300

    merge

---
+++ app/server.py
@@ -0,0 +1,33 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+#pickle model
+model_file = open("model.pkl","rb")
+model = pickle.load(model_file)
+
+classes = ['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy','Blueberry___healthy','Cherry_(including_sour)___Powdery_mildew','Cherry_(including_sour)___healthy','Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust_','Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Grape___Black_rot','Grape___Esca_(Black_Measles)','Grape___Leaf_blight_(Isariopsis_Leaf_Spot)','Grape___healthy','Orange___Haunglongbing_(Citrus_greening)','Peach___Bacterial_spot','Peach___healthy','Pepper,_bell___Bacterial_spot','Pepper,_bell___healthy','Potato___Early_blight','Potato___Late_blight','Potato___healthy','Raspberry___healthy','Soybean___healthy','Squash___Powdery_mildew','Strawberry___Leaf_scorch','Strawberry___healthy','Tomato___Bacterial_spot','Tomato___Early_blight','Tomato___Late_blight','Tomato___Leaf_Mold','Tomato___Septoria_leaf_spot','Tomato___Spider_mites Two-spotted_spider_mite','Tomato___Target_Spot','Tomato___Tomato_Yellow_Leaf_Curl_Virus','Tomato___Tomato_mosaic_virus','Tomato___healthy','background']
+
+@app.post("/analyze")
+async def analyze(request:Request ,img_data:bytes=File(...)):
+    #img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = model.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+@app.get("/info")
+def get_disease():
+    return {"Info": "Disease information"}
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=8080)
